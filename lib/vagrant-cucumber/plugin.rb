7b7c05d20d50a790882f80ac6ebd59d18075f1c1
David O'Rourke <david@scalefactory.com>
2016-07-18 12:00:09 +0100

    Remove multiprovider-snap require and rubocop cleanups in plugins.rb

--- lib/vagrant-cucumber/plugin.rb
+++ lib/vagrant-cucumber/plugin.rb
@@ -1,26 +1,22 @@
 begin
-    require "vagrant"
+    require 'vagrant'
 rescue LoadError
-    raise "The Vagrant Cucumber plugin must be run within Vagrant."
+    raise 'The Vagrant Cucumber plugin must be run within Vagrant.'
 end
 
 module VagrantPlugins
-    module Cucumber 
-        class Plugin < Vagrant.plugin("2")
-
-            require 'vagrant-multiprovider-snap'
-
-            name "Cucumber"
+    module Cucumber
+        class Plugin < Vagrant.plugin('2')
+            name 'Cucumber'
 
             description <<-DESC
             This plugin makes it possible for Cucumber to interact with Vagrant
             DESC
 
-            command "cucumber" do
-                require_relative "commands/cucumber"
+            command 'cucumber' do
+                require_relative 'commands/cucumber'
                 CucumberCommand
             end
-
         end
     end
 end
